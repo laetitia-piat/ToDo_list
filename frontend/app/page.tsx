d5688a7098a437aca1979d10e9f1fa4826f994ca
Laetitia <laeptitchat29@gmail.com>
2026-02-05 15:40:58 +0100

    delete folder

---
+++ frontend/app/page.tsx
@@ -0,0 +1,15 @@
+export default async function Home() {
+  const response = await fetch("http://localhost:8000/todos");
+  const data = await response.json();
+  console.log(data);
+  return (
+    <div>
+      <h1>My To-Do List</h1>
+      <ul>
+        {data.map((todo: any) => (
+          <li key={todo.id}>{todo.object}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
